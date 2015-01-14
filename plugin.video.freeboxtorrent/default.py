32211692a5f26668271dc8fb534c0dd29f9e5499
Fazzani <heni.fazzani@leetchi.com>
2015-01-15 00:05:26 +0100

    HFA

--- plugin.video.freeboxtorrent/default.py
+++ plugin.video.freeboxtorrent/default.py
@@ -1,24 +1,12 @@
-#-*- coding: utf-8 -*-
-import sys
-import xbmc
-import xbmcvfs
-import xbmcaddon
-import utils
-import time
-from datetime import datetime
-import re
-import urlparse
-
-__addon__ = xbmcaddon.Addon(id='plugin.video.freeboxtorrent')
-__addonname__ = __addon__.getAddonInfo('name')
-__icon__ = __addon__.getAddonInfo('icon')
-
+import sys, os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources', 'site-packages'))
+from freebox import monkey_patches, plugin
 
 if __name__ == '__main__':
-    __delaySettings__ = __addon__.getSetting("delay")
-    __PathOfPlayList__ = __addon__.getSetting("path_input")
-    if(__PathOfPlayList__== "" ):
-      __PathOfPlayList__ = __DefaultPathOfPlayList__
-    params = urlparse.parse_qs('&'.join(sys.argv[1:]))
-    print(repr(params) + "  ___________________________")
- 
+    try:
+        plugin.run()
+    except Exception, e:
+        import xbmc
+        import traceback
+        map(xbmc.log, traceback.format_exc().split("\n"))
+        raise
