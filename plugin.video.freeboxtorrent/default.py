51e5957772413820a86f2c81f28f963a128d1404
Fazzani <heni.fazzani@leetchi.com>
2015-01-14 23:32:31 +0100

    HFA: freebox torrent

---
+++ plugin.video.freeboxtorrent/default.py
@@ -0,0 +1,24 @@
+#-*- coding: utf-8 -*-
+import sys
+import xbmc
+import xbmcvfs
+import xbmcaddon
+import utils
+import time
+from datetime import datetime
+import re
+import urlparse
+
+__addon__ = xbmcaddon.Addon(id='plugin.video.refreshplaylist')
+__addonname__ = __addon__.getAddonInfo('name')
+__icon__ = __addon__.getAddonInfo('icon')
+
+
+if __name__ == '__main__':
+    __delaySettings__ = __addon__.getSetting("delay")
+    __PathOfPlayList__ = __addon__.getSetting("path_input")
+    if(__PathOfPlayList__== "" ):
+      __PathOfPlayList__ = __DefaultPathOfPlayList__
+    params = urlparse.parse_qs('&'.join(sys.argv[1:]))
+    print(repr(params) + "  ___________________________")
+ 
