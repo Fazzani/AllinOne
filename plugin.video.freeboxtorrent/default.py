a815fc3baa86366d6903f65e2df910143abd715a
Fazzani <heni.fazzani@leetchi.com>
2015-01-15 09:17:00 +0100

    HFA

--- plugin.video.freeboxtorrent/default.py
+++ plugin.video.freeboxtorrent/default.py
@@ -1,12 +1,12 @@
 import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources', 'site-packages'))
-from freebox import monkey_patches, plugin
+from freebox import monkey_patches
+from xbmcswift2 import Plugin
 
 if __name__ == '__main__':
     try:
+        plugin = Plugin()
         plugin.run()
-    except Exception, e:
-        import xbmc
-        import traceback
-        map(xbmc.log, traceback.format_exc().split("\n"))
+    except Exception:
+        
         raise
