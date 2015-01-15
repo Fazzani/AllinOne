039240a352073b6d0d41c1cc3ab9f058e3819806
Fazzani <heni.fazzani@leetchi.com>
2015-01-15 19:54:14 +0100

    HFA

--- plugin.video.xbmctorrent/addon.py
+++ plugin.video.xbmctorrent/addon.py
@@ -1,6 +1,9 @@
 import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources', 'site-packages'))
-from xbmctorrent import monkey_patches, plugin
+from freebox import monkey_patches, plugin
+from xbmcswift2 import Plugin
+
+plugin = Plugin()
 
 if __name__ == '__main__':
     try:
