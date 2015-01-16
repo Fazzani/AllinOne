a9e5b0e28921aca94ad02cf21d60343c89dc2f71
Fazzani <heni.fazzani@leetchi.com>
2015-01-16 02:36:23 +0100

    HFA: Getting Session token

--- plugin.video.freeboxtorrent/common.py
+++ plugin.video.freeboxtorrent/common.py
@@ -1,19 +1,25 @@
 import sys
 import os
+from contextlib import contextmanager, closing
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources', 'site-packages'))
+from xbmcswift2 import Plugin
+from default import plugin
 
 FREEBOX_API = 'http://mafreebox.freebox.fr/api/v3'
 RESOURCES_PATH = os.path.join(os.path.dirname(sys.modules["__main__"].__file__), 'resources')
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.66 Safari/537.36"
-HEADERS={"X-Fbx-App-Auth":"","Host":FREEBOX_API}
+HEADERS={"X-Fbx-App-Auth":"","Host":FREEBOX_API,"Content-Type":"application/json"}
 APP_ID = "plugin.video.freeboxtorrent"
 
-def url_get(url, params={}, headers={}, method='AUTO'):
+def url_get(url, params={}, headers={"Content-Type":"application/json"}, method='AUTO'):
     import urllib2
     import urllib
-    from freebox import plugin
-
+    plugin.log.info(url)
+    plugin.log.info(headers)
+    plugin.log.info(params)
     if params and method != 'GET':
-        data = urllib.urlencode(params)
+        data = repr(params).replace('\'','"').replace(' ','')
+        plugin.log.info('data : xxxxxxxxxxxxxxxxx  '+data)
         req = urllib2.Request(url, data)
     else:
         url = "%s?%s" % (url, urllib.urlencode(params))
--- plugin.video.freeboxtorrent/default.py
+++ plugin.video.freeboxtorrent/default.py
@@ -1,11 +1,77 @@
+from common import *
+import xbmc, json
 import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources', 'site-packages'))
-from freebox import monkey_patches
 from xbmcswift2 import Plugin
+
+plugin = Plugin()
+
+@plugin.route("/")
+def listTorrent():
+    authorize()
+    listTorrents = requestFreebox(FREEBOX_API + "/downloads")
+    plugin.log.info(listTorrents)
+    for torrent in ListTorrent:
+       yield{
+            "label": torrent["name"],
+            "thumbnail": torrent["image"],
+            "path": plugin.url_for(torrent["view"])
+           }
+
+def authorize():
+    params = {"app_id": APP_ID,
+              "app_name": "freeboxtorrent",
+              "app_version": "0.0.1",
+              "device_name": "XBMC"}
+    auth = json.loads(url_get(FREEBOX_API + "/login/authorize/", params = params, method='POST'))
+    plugin.log.info('auth ____________'+repr(auth))
+    if auth['success']:
+        plugin.log.info('auth success : '+auth['result']['app_token'])
+        authStorage = plugin.get_storage(name='authStorage', file_format='json', TTL=None)
+        authStorage.update({'app_token': auth['result']['app_token']})
+
+        while 1 == 1:
+            res = json.loads(url_get(FREEBOX_API + "/login/authorize/%s" % auth['result']['track_id']))
+            if res['result']['status'] == 'granted':
+                plugin.notify('GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGranted')
+                authStorage['challenge'] = res['result']['challenge']
+                authStorage['password'] = getPassword(authStorage['app_token'], authStorage['challenge'])
+                authStorage.update({'session': getSession(APP_ID, authStorage['password'])})
+                plugin.log.info("session = "+authStorage.get('session'))
+                authStorage.sync()
+                break
+            
+            xbmc.sleep(1000)
+    else:
+        plugin.log.info('auth failed')
+
+def getSession(app_id,password):
+    #POST /api/v3/login/session/
+    res = url_get(FREEBOX_API + "/login/session/",params={"app_id":app_id,"password":password}, method = "POST")
+    print(repr(res))
+    if res["success"]:
+        return res["result"]
+    raise Exception('Getting token session failed')
+
+def getPassword(app_token,challenge):
+    from hashlib import sha1
+    import hmac
+    plugin.log.info("app_token : "+app_token)
+    plugin.log.info("app_token encoded: "+app_token.replace('\\','').encode())
+    plugin.log.info("challenge : "+challenge.replace('\\',''))
+    plugin.log.info("challenge modified: "+challenge.replace('\\',''))
+    hashed = hmac.new(app_token.replace('\\','').encode(), challenge.replace('\\',''), sha1)
+    # The signature
+    return hashed.hexdigest()
+
+def requestFreebox(path, params={}, method="GET"):
+    authStorage = plugin.get_storage(name='authStorage', file_format='json', TTL=None)
+    plugin.log.info('session : ----------- '+str(authStorage.get('session')))
+    HEADERS["X-Fbx-App-Auth"] = authStorage.get('session')['session_token']
+    return json.dumps(url_get(FREEBOX_API + "/login/session/", params, HEADERS, method))
 
 if __name__ == '__main__':
     try:
-        plugin = Plugin()
         plugin.run()
     except Exception:
         
